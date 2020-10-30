08ef2be1b4fa339939aabe5062a763c4d5177060
Jon Simington <jon.simington@mst.edu>
2020-10-30 14:13:06 -0500

    Add filters component to allow user to filter purchases

---
+++ src/Filters.js
@@ -0,0 +1,12 @@
+function Filters(props) {
+  return (
+    <div class="container bg-info">
+      <div class="row">
+        <div class="col-sm">Filter by Customer</div>
+        <div class="col-sm">Filter by Month</div>
+      </div>
+    </div>
+  );
+}
+
+export default Filters;
