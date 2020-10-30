cdbc5d50aa62a77c5a2b78504a26f866b68baf10
Jon Simington <jon.simington@mst.edu>
2020-10-30 14:13:16 -0500

    Run prettier on all existing files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Header from './ui/header/Header';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Filters from "./Filters";
+import "./index.css";
+import Purchases from "./Purchases";
+import Header from "./ui/header/Header";
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <div class="container-fluid bg-dark">
+      <Filters />
+      <Purchases />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
