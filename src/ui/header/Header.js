9b83a81a1820cafc95d3893bddd655a1cf9dfbaf
Jon Simington <jon.simington@mst.edu>
2020-10-30 12:50:39 -0500

    Add header component

---
+++ src/ui/header/Header.js
@@ -0,0 +1,17 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Rewards Program</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default Header;
