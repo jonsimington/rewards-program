e32b6f959d69edde8dfd418ada893159b7d1372f
Jon Simington <jon.simington@mst.edu>
2020-10-30 14:12:46 -0500

    Add base purchaseData array

---
+++ src/purchaseData.js
@@ -0,0 +1,12 @@
+const purchaseData = [
+  {
+    name: "Netflix Subscription",
+    total: -15.99,
+  },
+  {
+    name: "MARIA BONITA MEXICAN 10/21 PURCHASE ST PETERS MO",
+    total: -120,
+  },
+];
+
+export default purchaseData;
