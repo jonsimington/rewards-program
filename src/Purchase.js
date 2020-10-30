8bb691608af3878b2cb82dd55c7113f94d50a841
Jon Simington <jon.simington@mst.edu>
2020-10-30 14:29:18 -0500

    Use react-bootstrap & format purchases in a grid

--- src/Purchase.js
+++ src/Purchase.js
@@ -1,12 +1,29 @@
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 function Purchase(props) {
   return (
-    <Card>
-      <Card.Body>
-        {props.name} - {props.total} - {props.pointsEarned}
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row className="mb-2">
+        <Col sm={7}>
+          <Card>
+            <Card.Body>{props.name}</Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>{props.total}</Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>{props.pointsEarned}</Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/Purchases.js
+++ src/Purchases.js
@@ -1,3 +1,7 @@
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import Purchase from "./Purchase";
 import purchaseData from "./purchaseData";
 
@@ -18,7 +22,21 @@ for (const [index, value] of purchaseData.entries()) {
 }
 
 function Purchases(props) {
-  return purchases;
+  return (
+    <Card className="bg-light">
+      <Card.Body>
+        <Container fluid>
+          <Row className="font-weight-bold text-center">
+            <Col sm={7}>Name</Col>
+            <Col>Total</Col>
+            <Col>Rewards Points Earned</Col>
+          </Row>
+          <hr></hr>
+          {purchases}
+        </Container>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Purchases;
