4e6835e45d70f7c3e0c0d08495d9f1f5f109ee67
Jon Simington <jon.simington@mst.edu>
2020-10-30 13:39:12 -0500

    Create Purchase component to display data for individual purchases

---
+++ src/Purchase.js
@@ -0,0 +1,13 @@
+import Card from 'react-bootstrap/Card';
+
+function Purchase(props) {
+    return (
+        <Card>
+            <Card.Body>
+                {props.name} - {props.total} - {props.pointsEarned}
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Purchase;
